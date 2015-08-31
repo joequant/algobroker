51ee29689a0e479cb5005cbce81fd18e2b79645b
Joseph C Wang <joequant@gmail.com>
2015-08-31 15:14:32 +0800

    refactoring and name changes

--- startup.sh
+++ startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-python3 algobroker/plivo_broker.py &
+python3 algobroker/broker_plivo.py &
 python3 algobroker/dispatcher.py &
-python3 algobroker/alert.py &
-python3 algobroker/yahoo_quoter.py &
+python3 algobroker/strategy_alert.py &
+python3 algobroker/quoter_yahoo.py &
 
 
