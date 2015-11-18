808a3563e2d4d568df4a57a9d8ee91b921640026
Joseph C Wang <joequant@gmail.com>
2015-11-18 14:07:15 +0800

    remove desk alert

--- start-algo.sh
+++ start-algo.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 python3 algobroker/broker_plivo.py &
 python3 algobroker/broker_bitmex.py &
-python3 algobroker/broker_desk_alert.py &
 python3 algobroker/broker_twilio.py &
 python3 algobroker/dispatcher.py &
 python3 algobroker/strategy_alert.py &
